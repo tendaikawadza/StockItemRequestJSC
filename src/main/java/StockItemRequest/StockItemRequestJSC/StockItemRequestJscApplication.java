c1045b4be9531b2a533cd0489076cc5531cdcb94
Tendai Kawadza <tendaikawadza49@gmail.com>
2022-10-28 03:46:36 -0700

    microservice stockitemrequest

---
+++ src/main/java/StockItemRequest/StockItemRequestJSC/StockItemRequestJscApplication.java
@@ -0,0 +1,13 @@
+package StockItemRequest.StockItemRequestJSC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockItemRequestJscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockItemRequestJscApplication.class, args);
+	}
+
+}
